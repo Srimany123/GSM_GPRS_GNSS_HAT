b9b73d852d2bf72ce3939a8bba26a74945b7b502
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2024-08-18 12:47:20 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,4 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python3-serial python3-rpi.gpio
 sudo apt-get install ppp minicom
-
-sudo apt-get update
 sudo apt-get install minicom
-
-
